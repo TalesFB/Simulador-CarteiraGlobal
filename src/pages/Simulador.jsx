982d0e230afe6d56c9ed96eb7f2fe0ebb9ad66df
Tales Ferreira <tales.ifnmg@gmail.com>
2021-02-04 12:40:29 -0300

    primeiro comit

---
+++ src/pages/Simulador.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { AiFillQuestionCircle } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+
+import { GoGraph } from "react-icons/go";
+import { RiWallet2Line } from "react-icons/ri";
+import {} from "react-icons/";
+import "./styles.css";
+
+function Simulador() {
+  return (
+    <div id="page-landing">
+      <div className="sidebar"></div>
+      <div id="main">
+        <div className="topbar">
+          <AiFillQuestionCircle />
+          <FaMoon />
+          <img
+            src="https://i.pinimg.com/originals/8b/da/ca/8bdaca81d5ddbaeb92b61d6b5787d866.jpg"
+            alt=""
+          />
+        </div>
+
+        <h3>RENDA FIXA</h3>
+        <h1>Simulador de Renda fixa</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Simulador;
