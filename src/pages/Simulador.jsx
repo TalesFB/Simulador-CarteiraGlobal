21da9b0956d4ca6e5b95b605f5ab656beeaf75b2
Tales Ferreira <tales.ifnmg@gmail.com>
2021-02-09 19:22:59 -0300

    modificação do grafico em highcharts

--- src/pages/Simulador.jsx
+++ src/pages/Simulador.jsx
@@ -15,6 +15,7 @@ function Simulador() {
   const options = {
     chart: {
       type: "column",
+      spacing: [20, 40, 20, 20],
     },
     credits: {
       enabled: false,
@@ -22,31 +23,67 @@ function Simulador() {
     title: {
       text: "",
     },
+    legend: {
+      enabled: false,
+    },
+    plotOptions: {
+      series: {
+        dataLabels: {
+          enabled: true,
+          useHTML: true,
+          formatter: function () {
+            return `Rendimento<br/>${Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            }).format(parseFloat(this.y) * 36000)}`;
+          },
+        },
+      },
+    },
     colors: ["#8c979f", "#447cff", "#5ac7ff", "#e4e6e8"],
     xAxis: {
-      visible: false,
-      lineColor: "transparent",
-      tickLength: 0,
+      // visible: false,
+      // lineColor: "transparent",
+      // tickLength: 0,
+      labels: {
+        formatter: function () {
+          return `${
+            this.value
+          } <br/><strong style="font-size: 15px">${Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          }).format(
+            this.chart.series[0].yData[this.pos] * 36000 + 100000
+          )}</strong>`;
+        },
+      },
+      categories: ["TOTAL LÍQUIDO", "TOTAL LÍQUIDO", "TOTAL LÍQUIDO"],
     },
     yAxis: {
       title: {
-        text: "",
+        text: null,
       },
-      gridLineWidth: 0,
-      minorGridLineWidth: 0,
+      opposite: true,
+      labels: {
+        align: "right",
+        formatter: function () {
+          return Intl.NumberFormat("pt-BR", {
+            style: "percent",
+            maximumFractionDigits: 0,
+          }).format(this.value);
+        },
+      },
+      // gridLineWidth: 0,
+      // minorGridLineWidth: 0,
     },
     series: [
       {
         name: "TOTAL LÍQUIDO",
-        data: [0.5],
-      },
-      {
-        name: "TOTAL LÍQUIDO",
-        data: [0.7],
-      },
-      {
-        name: "TOTAL LÍQUIDO",
-        data: [0.8],
+        data: [0.5, 0.7, 0.8],
       },
     ],
   };
